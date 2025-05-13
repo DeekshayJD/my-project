722d5d4c89409a51dfdaf32f31a4e5c90cdec91b
deekshay <deekshay8145@gmail.com>
2025-05-13 21:46:27 +0530

    added non repating character in string

--- code.py
+++ code.py
@@ -9,3 +9,14 @@ def find_duplicates(l):
 
 l=[1,6,5,4,3,2,3]
 print(find_duplicates(l))
+
+
+# find first non repating character in string
+
+s="NETSETOSNET"
+s1=[]
+for i in s:
+   if i not in s1 and s.count(i)<2:
+      s1.append(i)
+result=" ".join(s1)
+print(result)
